d3518671d7a193b07baf4bb68ff2bb15f5e0bb20
victorpereira <dev.victorhugo@gmail.com>
2022-02-09 23:19:13 -0300

    feat: Protected Route

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import {UserStorage} from './UserContext';
+import User from './Components/User/User';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <UserStorage>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
         </UserStorage>
       </BrowserRouter>
     </div>
--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { USER_POST } from '../../api';
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
+import Error from '../Helper/Error';
 import Input from '../Forms/Input';
 import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch';
+
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
   const password = useForm('password');
 
   const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -18,9 +22,10 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
+
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
@@ -28,7 +33,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="username" {...email} />
         <Input label="Senha" type="password" name="username" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
